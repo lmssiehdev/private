aec94d7731dbd1427bdd7354907971804902522e
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2023-05-16 16:21:14 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -83,7 +83,7 @@ app.post("/api/user/get_user_prestige", (res) => {
 
 app.post("/api/find_game", (res) => {
     readPostedJson(res, (body) => {
-        const addr = "customsurviv.fly.dev:444";
+        const addr: string = Config.useWebSocketDevAddress ? Config.webSocketDevAddress : Config.webSocketRegions[body?.region] ?? Config.webSocketRegions[Config.defaultRegion];
         res.writeHeader("Content-Type", "application/json");
         res.end(JSON.stringify({ res: [{ zone: body.zones[0], gameId: "", useHttps: Config.useHttps, hosts: [addr], addrs: [addr] }] }));
     }, () => {
